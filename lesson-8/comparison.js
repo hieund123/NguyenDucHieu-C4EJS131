3acdb69dd3064af8f03c10f588650899a94d9610
hieund123 <butbisanh@gmail.com>
2022-08-18 20:29:13 +0700

    nguyenduchieu-c4ej131

---
+++ lesson-8/comparison.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 15;
+//a
+console.log(a > b);
+
+//b
+console.log(a == 10);
+
+//c
+console.log(typeof (a == 10));
+
+let x = 5;
+let y = 6;
+console.log(x!==y);
---
+++ lesson-8/index.js
@@ -0,0 +1,26 @@
+// console.log(typeof(123));
+
+let x = 7;
+
+console.log(x > 10, x > 6);
+
+console.log(x >= 10, x >= 7);
+
+let max = 3;
+for (let i = 1; i <= max; i++) {
+  console.log(i);
+}
+
+let i = 1;
+while (i <= max) {
+    console.log(i);
+    i++;
+}
+
+
+let n = 1;
+do {
+    console.log(n);
+    n++;
+}
+while (n <= max)
