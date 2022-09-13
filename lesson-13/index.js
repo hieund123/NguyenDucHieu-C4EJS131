48639695c0ef6491b182c4249806821b5ee81802
hieund123 <butbisanh@gmail.com>
2022-09-13 23:09:21 +0700

    nguyenduchieu-c4ejs131

---
+++ lesson-13/index.js
@@ -0,0 +1,7 @@
+// const text = '["1" , "2" , "Hello", ]';
+// const parsejson = JSON.parse(text);
+// document.getElementById("id1").innerHTML = parsejson;
+
+const obj = {"name": "quan","age": 26,"address": "hn"};
+const parsejson1 = JSON.stringify(obj);
+document.getElementById("id2").innerHTML = parsejson1;
